8cc39f446579a653dff231480fdf778539876ca1
Aditya Gupta <ag15035@gmail.com>
2021-07-01 12:59:04 +0530

    Bookkeeping: Attempt to fix makefile [Failed]

--- Makefile
+++ Makefile
@@ -16,19 +16,15 @@ BOOTEFI := $(BUILDDIR)/bootloader.efi
 LDS = $(SRCDIR)/kernel.ld
 
 all:
-	@-printf "Building Kernel...\r"
-	@-d=$$(date +%s);\
-        make build;\
-	time=$$(($$(date +%s)-d));\
-	if [ $$time -eq 1 ]; then\
-		echo "Kernel took $$((time)) second to compile!";\
-	else\
-		echo "Kernel took $$((time)) seconds to compile!";\
-	fi
-	@-printf "Making ISO...     \r"
-	@-make image
-	@-printf "Starting Qemu...  \r"
-	@-make run
+	echo "Building Kernel..."
+	d=$(date +%s);
+	make build;
+	time=$(($(date +%s)-d));
+	echo "Kernel took $((time)) seconds to compile!";
+	echo "Making ISO...     \r"
+	make image
+	echo "Starting Qemu...  \r"
+	make run
 
 rwildcard=$(foreach d,$(wildcard $(1:=/*)),$(call rwildcard,$d,$2) $(filter $(subst *,%,$2),$d))
 
@@ -41,53 +37,54 @@ DIRS = $(wildcard $(SRCDIR)/*)
 .PHONY: build
 
 build:
-	@-if [ `head -n 1 hbm` = "OS=macOS" ]; then\
-		docker exec HBM /bin/bash -c "cd /home/HydraOS/;make kernel";\
-	elif [ `head -n 1 hbm` = "OS=Debian" ]; then\
-		make kernel;\
-	else\
-		echo "Please run ./setup-environment.sh";\
+	if [ `head -n 1 hbm` = "OS=macOS" ]; then
+		docker exec HBM /bin/bash -c "cd /home/HydraOS/;make kernel";
+	elif [ `head -n 1 hbm` = "OS=Debian" || `head -n 1 hbm` = "OS=Arch" ]; then
+		make kernel;
+	else
+		echo "Please run ./setup-environment.sh";
 	fi
 
 kernel: $(OBJS) link
 
 $(OBJDIR)/drivers/interrupts/interrupts.o: $(SRCDIR)/drivers/interrupts/interrupts.cpp
-	@-mkdir -p $(@D)
-	@-$(CC) -mno-red-zone -mgeneral-regs-only -ffreestanding -c $^ -o $@
+	mkdir -p $(@D)
+	$(CC) -mno-red-zone -mgeneral-regs-only -ffreestanding -c $^ -o $@
 
 $(OBJDIR)/%.o: $(SRCDIR)/%.cpp
-	@-mkdir -p $(@D)
-	@-$(CC) $(CFLAGS) -c $^ -o $@
+	mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $^ -o $@
 	
 $(OBJDIR)/%_asm.o: $(SRCDIR)/%.asm
-	@-mkdir -p $(@D)
-	@-$(ASMC) $(ASMFLAGS) $^ -f elf64 -o $@
+	mkdir -p $(@D)
+	$(ASMC) $(ASMFLAGS) $^ -f elf64 -o $@
 	 
 link:
-	@-$(LD) $(LDFLAGS) -o $(BUILDDIR)/kernel.elf $(OBJS)
+	$(LD) $(LDFLAGS) -o $(BUILDDIR)/kernel.elf $(OBJS)
 
 image:
-	@-dd if=/dev/zero of=$(BUILDDIR)/Hydra.iso bs=512 count=93750 > /dev/null 2>&1
-	@-mformat -i $(BUILDDIR)/Hydra.iso -f 1440 ::
-	@-mmd -i $(BUILDDIR)/Hydra.iso ::/EFI
-	@-mmd -i $(BUILDDIR)/Hydra.iso ::/EFI/BOOT
-	@-mcopy -i $(BUILDDIR)/Hydra.iso $(BOOTEFI) ::/EFI/BOOT
-	@-mcopy -i $(BUILDDIR)/Hydra.iso $(BUILDDIR)/startup.nsh ::
-	@-mcopy -i $(BUILDDIR)/Hydra.iso $(BUILDDIR)/kernel.elf ::
-	@-mcopy -i $(BUILDDIR)/Hydra.iso $(BUILDDIR)/font.psf ::
+	dd if=/dev/zero of=$(BUILDDIR)/Hydra.iso bs=512 count=93750 > /dev/null 2>&1
+	mformat -i $(BUILDDIR)/Hydra.iso -f 1440 ::
+	mmd -i $(BUILDDIR)/Hydra.iso ::/EFI
+	mmd -i $(BUILDDIR)/Hydra.iso ::/EFI/BOOT
+	mcopy -i $(BUILDDIR)/Hydra.iso $(BOOTEFI) ::/EFI/BOOT
+	mcopy -i $(BUILDDIR)/Hydra.iso $(BUILDDIR)/startup.nsh ::
+	mcopy -i $(BUILDDIR)/Hydra.iso $(BUILDDIR)/kernel.elf ::
+	mcopy -i $(BUILDDIR)/Hydra.iso $(BUILDDIR)/font.psf ::
 
 run:
-	@-if [ `head -n 1 hbm` = "OS=macOS" ]; then\
-		qemu-system-x86_64 -drive file=$(BUILDDIR)/Hydra.iso,format=raw -m $(RAM) -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="$(OVMFDIR)/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="$(OVMFDIR)/OVMF_VARS-pure-efi.fd" -net none -boot order=d;\
-	elif [ `head -n 1 hbm` = "OS=Debian" ]; then\
-		if [ -d "/mnt/c/" ]; then\
-			cmd.exe /C qemu-system-x86_64 -drive file=$(BUILDDIR)/Hydra.iso,format=raw -m $(RAM) -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=$(OVMFDIR)/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=$(OVMFDIR)/OVMF_VARS-pure-efi.fd -net none -boot order=d;\
-		else\
-			qemu-system-x86_64 -drive file=$(BUILDDIR)/Hydra.iso,format=raw -m $(RAM) -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="$(OVMFDIR)/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="$(OVMFDIR)/OVMF_VARS-pure-efi.fd" -net none -boot order=d;\
-		fi;\
-	else\
-		echo "Please run ./setup-environment.sh";\
+	if [ `head -n 1 hbm` = "OS=macOS" ]; then
+		qemu-system-x86_64 -drive file=$(BUILDDIR)/Hydra.iso,format=raw -m $(RAM) -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="$(OVMFDIR)/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="$(OVMFDIR)/OVMF_VARS-pure-efi.fd" -net none -boot order=d;
+	elif [ `head -n 1 hbm` = "OS=Debian" || `head -n 1 hbm` = "OS=Arch" ]; then
+		# Looks bit dangerous
+		if [ -d "/mnt/c/" ]; then
+			cmd.exe /C qemu-system-x86_64 -drive file=$(BUILDDIR)/Hydra.iso,format=raw -m $(RAM) -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=$(OVMFDIR)/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=$(OVMFDIR)/OVMF_VARS-pure-efi.fd -net none -boot order=d;
+		else
+			qemu-system-x86_64 -drive file=$(BUILDDIR)/Hydra.iso,format=raw -m $(RAM) -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="$(OVMFDIR)/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="$(OVMFDIR)/OVMF_VARS-pure-efi.fd" -net none -boot order=d;
+		fi;
+	else
+		echo "Please run ./setup-environment.sh";
 	fi
 
 clean:
-	@-rm -r Build/Binaries/*
+	rm -r Build/Binaries/*
--- setup-environment.sh
+++ setup-environment.sh
@@ -14,37 +14,37 @@ mkdir Hydra bin > /dev/null 2>&1
 if [ $OS -eq 0 ]; then
     which -s brew
     if [[ $? != 0 ]] ; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing Homebrew...\r $X "; sleep 0.1; done; done
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing Homebrew... $X "; sleep 0.1; done; done
     else
-        brew update > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Updating Homebrew...\r $X "; sleep 0.1; done; done
+        brew update > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Updating Homebrew... $X "; sleep 0.1; done; done
     fi
     which -s docker
     if [[ $? != 0 ]] ; then
-        brew install docker > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing Docker...\r $X "; sleep 0.1; done; done
+        brew install docker > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing Docker... $X "; sleep 0.1; done; done
     fi
     open -g -a Docker.app || exit
-    docker system info > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Starting Docker...     \r $X "; sleep 0.1; done; done
+    docker system info > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Starting Docker... $X "; sleep 0.1; done; done
     if docker images | grep -q 'hbm'; then
-		printf "HBM Image already exists...           \r";
+		echo "HBM Image already exists...";
     else
-        docker pull firelscar/hbm:latest > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Pulling HBM Image...     \r $X "; sleep 0.1; done; done
+        docker pull firelscar/hbm:latest > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Pulling HBM Image... $X "; sleep 0.1; done; done
     fi
     if docker ps -a | grep -q 'hbm'; then
-		printf "Container already exists...              \r";
+		echo "Container already exists...";
     else
-        docker run -d -v $WhereAmI:/home/HydraOS/ --name HBM firelscar/hbm:latest tail -f /dev/null > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Creating HBM Container...     \r $X "; sleep 0.1; done; done
+        docker run -d -v $WhereAmI:/home/HydraOS/ --name HBM firelscar/hbm:latest tail -f /dev/null > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Creating HBM Container... $X "; sleep 0.1; done; done
     fi
-    printf "Checking macOS dependencies...\r";
+    echo "Checking macOS dependencies...";
     which -s mtools
     if [[ $? != 0 ]] ; then
-        brew install mtools > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing mtools...     \r $X "; sleep 0.1; done; done
+        brew install mtools > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing mtools... $X "; sleep 0.1; done; done
     fi
     which -s qemu-system-x86_64
     if [[ $? != 0 ]] ; then
-        brew install qemu > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing qemu...     \r $X "; sleep 0.1; done; done
+        brew install qemu > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing qemu... $X "; sleep 0.1; done; done
     fi
     echo -e "OS=macOS" > hbm
-    printf "Done!                            \n";
+    echo "Done!";
     echo "Build and run with the 'make' command"
     exit
 fi
@@ -62,36 +62,45 @@ if [ "$(uname -s)" = "Linux" ]; then
 	fi
 fi
 if [ $DIST == "Ubuntu" ] || [ $DIST == "Debian" ]; then
-    printf "Checking for $DIST dependencies...           \r";
+    echo "Checking for $DIST dependencies...";
     apt update > /dev/null 2>&1
 	which -a sudo > /dev/null 2>&1
     if [[ $? != 0 ]] ; then
-        apt install sudo -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing sudo...                  \r $X "; sleep 0.1; done; done
+        apt install sudo -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing sudo... $X "; sleep 0.1; done; done
     fi
 	sudo apt update > /dev/null 2>&1
 	which -a make > /dev/null 2>&1
     if [[ $? != 0 ]] ; then
-        sudo apt install make -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing make...              \r $X "; sleep 0.1; done; done
+        sudo apt install make -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing make... $X "; sleep 0.1; done; done
     fi
 	which -a gcc > /dev/null 2>&1
     if [[ $? != 0 ]] ; then
-        sudo apt install build-essential -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing build-essential...              \r $X "; sleep 0.1; done; done
+        sudo apt install build-essential -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing build-essential... $X "; sleep 0.1; done; done
     fi
 	which -a nasm > /dev/null 2>&1
     if [[ $? != 0 ]] ; then
-        sudo apt install nasm -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing nasm...                  \r $X "; sleep 0.1; done; done
+        sudo apt install nasm -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing nasm... $X "; sleep 0.1; done; done
     fi
 	which -a mtools > /dev/null 2>&1
     if [[ $? != 0 ]] ; then
-        sudo apt install mtools -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing mtools...                   \r $X "; sleep 0.1; done; done
+        sudo apt install mtools -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing mtools... $X "; sleep 0.1; done; done
     fi
 	which -a qemu-system-x86_64 > /dev/null 2>&1
     if [[ $? != 0 ]] ; then
-            sudo apt install qemu-system -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing qemu-system...                  \r $X "; sleep 0.1; done; done
+            sudo apt install qemu-system -y > /dev/null & while [ "$(ps a | awk '{print $1}' | grep $!)" ] ; do for X in '-' '/' '|' '\'; do echo -en "Installing qemu-system... $X "; sleep 0.1; done; done
             sudo apt install qemu-system -y > /dev/null 2>&1
     fi
     echo -e "OS=Debian" > hbm
-    printf "Done!                            \n";
+    echo "Done!";
+    echo "Build and run with the 'make' command"
+    exit
+
+elif [ $DIST == "Arch" ]; then
+    echo "Checking for $DIST dependencies...";
+    # Not checking for sudo
+    sudo pacman -Sy --needed base-devel nasm mtools qemu
+    echo -e "OS=Arch" > hbm
+    echo "Done!";
     echo "Build and run with the 'make' command"
     exit
 fi
